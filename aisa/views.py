d986f59d24ec96cec8c12a88e7ee48abfaf9f364
ashishthakur98 <53056815+ashishthakur98@users.noreply.github.com>
2019-07-20 13:06:32 +0530

    Update views.py

--- aisa/views.py
+++ aisa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+class Study(models.Model):
+    study_title = models.CharField(max_length=200)
+    study_content = models.TextField()
+    study_published = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.tutorial_title
+
