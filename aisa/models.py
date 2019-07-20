dcbf6700cad8981738c325cc0f5ab1ac106dfec1
ashishthakur98 <53056815+ashishthakur98@users.noreply.github.com>
2019-07-20 12:12:56 +0530

    Update models.py
    created a study class

--- aisa/models.py
+++ aisa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Study(models.Model):
+      study_tittle = models.Charfield(max_length=150)
+      study_content = models.TextField()
+      study_published = models.DateTimeField('date published')
+      
+      
+      def __str__(self):
+        return self.study_tittle
+  
